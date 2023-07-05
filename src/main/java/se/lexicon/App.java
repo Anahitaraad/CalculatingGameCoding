e531d0e8d89547e5bb5a80f5d55cce9fbeb70446
AnnA <anahita2012rad@gmail.com>
2023-07-06 00:58:41 +0200

    adding ascii codes

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -6,5 +6,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-    }
+
+        char m = '\u0000';
+        char x = '\uffff';
+
+        System.out.println(m);
+        System.out.println(x);
+
+            }
 }
