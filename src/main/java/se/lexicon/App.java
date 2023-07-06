4ca62cfe1be7844aa954f224264058ce25cb3de3
AnnA <anahita2012rad@gmail.com>
2023-07-07 01:22:38 +0200

    calculator

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,10 +1,10 @@
 package se.lexicon;
 
+import java.util.Scanner;
 
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
 
         /*
         System.out.println( "Hello World!" );
@@ -20,6 +20,7 @@ public class App
 
          */
 
+       /*
         Person anna = new Person();
         anna.firstName = "AnnA";
         anna.lastName = "Raad";
@@ -33,7 +34,28 @@ public class App
         amir.age = 40;
         amir.hobby = "game";
         System.out.println(amir.getInformation());
+        */
 
+        Scanner scanner = new Scanner(System.in);
 
-            }
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+        int multiplication = num1 * num2;
+        int division = num1 / num2;
+        int subtraction = num1 - num2;
+
+        System.out.println(num1 + " + " + num2 + " = " + sum);
+        System.out.println(num1 + " * " + num2 + " = " + multiplication);
+        System.out.println(num1 + " / " + num2 + " = " + division);
+        System.out.println(num1 + " - " + num2 + " = " + subtraction);
+
+    }
 }
+
+
+
