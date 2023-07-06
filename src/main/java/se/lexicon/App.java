a3d3bf62d3634f19154dafeda62df414706b7617
AnnA <anahita2012rad@gmail.com>
2023-07-06 22:17:08 +0200

    adding

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -7,7 +7,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("Hello");
-        System.out.println("Ali!");
+        System.out.println("AnnA!");
 
 
         char m = '\u0000';
