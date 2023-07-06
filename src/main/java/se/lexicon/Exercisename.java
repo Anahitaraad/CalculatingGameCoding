3380779346c8742af718b0874071251753677532
AnnA <anahita2012rad@gmail.com>
2023-07-06 20:26:35 +0200

    exercise.name

---
+++ src/main/java/se/lexicon/Exercisename.java
@@ -0,0 +1,9 @@
+package se.lexicon;
+public class Exercisename {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("AnnA!");
+
+
+    }
+}
