3a5c4c0c425aaaa28c207dd64d8c1909caf113d0
AnnA <anahita2012rad@gmail.com>
2023-07-06 22:16:21 +0200

    add

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -6,6 +6,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("Hello");
+        System.out.println("Ali!");
+
 
         char m = '\u0000';
         char x = '\uffff';
