8c80e32e8ae4a2b2367abe34065f6470a97bb591
AnnA <anahita2012rad@gmail.com>
2023-07-10 23:42:26 +0200

    leapyear

---
+++ src/main/java/se/lexicon/LeapYearChecker.java
@@ -0,0 +1,19 @@
+package se.lexicon;
+
+import java.util.Scanner;
+public class LeapYearChecker {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter a year: ");
+            int year = scanner.nextInt();
+
+            boolean isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+            if (isLeapYear) {
+                System.out.println(year + " is a leap year.");
+            } else {
+                System.out.println(year + " is not a leap year.");
+            }
+        }
+    }
+
