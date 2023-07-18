3222a85240d4ddd7911aaf4534da639e033582a9
AnnA <anahita2012rad@gmail.com>
2023-07-19 00:02:21 +0200

    StringLength

---
+++ src/main/java/exercises/StringLength.java
@@ -0,0 +1,20 @@
+package exercises;
+
+/* What is the length of the String: "Java" */
+
+public class StringLength {
+    public static void main(String[] args) {
+        String str = "Java";
+        int length = str.length();
+        System.out.println("Length of the string: " + length);
+    }
+}
+
+
+
+
+
+
+
+
+
